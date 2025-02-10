d5f134eee2b8eee100f26bf9292f8ab784bd564d
Rommanowski <jakub.romanowski17@gmail.com>
2025-02-10 22:51:20 +0100

    added flask app

---
+++ app/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, redirect, url_for, render_template, request
+from utils import *
+
+app = Flask(__name__)
+
+model = Net().to(device)
+model.load_state_dict(torch.load("model_inverted_dict.pth", weights_only=True))
+model.eval()
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+    
+@app.route('/compute/', methods=['POST'] )
+def compute():
+    if request.method == 'POST':
+        value = request.form['value']
+        if value=="":
+            value="1"*128*128
+        try:
+            output = string_to_index(model, value)
+            return output
+        except:
+            return f"some error with passing the value through the model"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ app/utils.py
@@ -0,0 +1,125 @@
+# imports
+import torch
+import torch.nn as nn
+from torchvision import transforms
+import numpy as np
+from PIL import Image
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# model
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.feature_extractor = nn.Sequential(  
+            #0
+            #1x28x28
+            # 1
+            nn.Conv2d(1, 32, kernel_size=3, padding=1), 
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),   #32x14x14
+            # 2
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),     #64x14x14
+            # 3
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2),    #128x7x7
+            #4
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU()    #128x7x7
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128*7*7, 512),
+            nn.ReLU(),
+            nn.Dropout(0.25),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Dropout(0.25),
+            nn.Linear(256, 35)
+        )
+        
+    def forward(self, x):
+        x = x.view(-1, 1, 28, 28)  # Reshape input to (batch_size, 1, 28, 28)
+        x = 1.0 - x
+        x = self.feature_extractor(x)
+        x = self.classifier(x)
+        x = x.squeeze()
+        return x
+    
+# this function gets rid of very light pixels, and darkens the dark grey ones, used to make the input more machine-friendly
+def sharpen_image(tensor, upper_treshold=0.75, lower_treshold=0.25):
+    # Ustawiamy piksele < 0.5 na 0
+    tensor[tensor > upper_treshold] = 1
+    tensor[tensor < lower_treshold] = 0
+    return tensor
+
+# transforming 128x128 PIL image to 28x28 tensor
+# ( just reshaping seemed to loose a lot of quality )
+user_transforms=transforms.Compose([
+    transforms.Grayscale(num_output_channels=1),
+    transforms.ToTensor(),
+    transforms.Lambda(lambda x: x.to(device)),
+    transforms.Lambda(lambda x: 1-x),
+    nn.MaxPool2d(kernel_size=2),
+    transforms.Lambda(lambda x: sharpen_image(x, 0.7, 0.4)),
+    #nn.MaxPool2d(kernel_size=2),
+    transforms.Resize((28,28)),
+    transforms.Lambda(lambda x: 1-x),
+    transforms.Lambda(lambda x: sharpen_image(x, 0.6, 0.4)),
+])
+
+# id's dictionary
+idx_to_class = {
+    0: 'airplane',
+    1: 'apple',
+    2: 'banana',
+    3: 'bat',
+    4: 'cow',
+    5: 'dog',
+    6: 'door',
+    7: 'eye',
+    8: 'fork',
+    9: 'guitar',
+    10: 'hexagon',
+    11: 'house',
+    12: 'ice-cream',
+    13: 'key',
+    14: 'moon',
+    15: 'panda',
+    16: 'pants',
+    17: 'pizza',
+    18: 'radio',
+    19: 'school-bus',
+    20: 'scissors',
+    21: 'skateboard',
+    22: 'snail',
+    23: 'snowflake',
+    24: 'snowman',
+    25: 'spoon',
+    26: 'strawberry',
+    27: 'streetlight',
+    28: 'submarine',
+    29: 'swan',
+    30: 'sword',
+    31: 'teddy-bear',
+    32: 'toilet',
+    33: 'watermelon',
+    34: 'zigzag'
+}
+
+def string_to_index(model, string_image):
+    array_image = np.zeros(len(string_image))
+    for i in range(len(string_image)):
+        array_image[i] = string_image[i] * 255
+    array_image = array_image.reshape(128,128)
+    array_image = Image.fromarray(array_image)
+    array_image = user_transforms(array_image)
+    output = model(array_image)
+    index_predicted = np.argmax(output.detach().cpu().numpy().squeeze())
+    return idx_to_class[index_predicted]
