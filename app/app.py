2feebe9427c287267eff36c23134bf24de35c65a
Rommanowski <jakub.romanowski17@gmail.com>
2025-02-25 01:35:00 +0100

    updated the model

--- app/app.py
+++ app/app.py
@@ -3,13 +3,13 @@ from utils import *
 
 app = Flask(__name__)
 
-model = Net().to(device)
-model.load_state_dict(torch.load("model_extended_temp_dict.pth", weights_only=True))
-model.eval()
-
-# model = torch.load("model_extended_temp.pth", weights_only=False)
-# torch.save(model.state_dict(), "model_extended_temp_dict.pth")
+# model = Net().to(device)
+# model.load_state_dict(torch.load("model_extended_final_dict.pth", weights_only=True))
 # model.eval()
+
+model = torch.load("model_extended_final.pth", weights_only=False)
+torch.save(model.state_dict(), "model_extended_final_dict.pth")
+model.eval()
 
 
 @app.route('/')
